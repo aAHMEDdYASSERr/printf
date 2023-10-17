e98346f3604523c82187b8d5ff6dfe14bf5b2840
ahmeed <ema@gmail.com>
2023-10-17 09:45:49 -0700

    a

---
+++ main.h
@@ -0,0 +1,86 @@
+
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define NULL_STRING "(null)"
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag
+ *
+ * @plus_flag: on if plus_flag
+ * @space_flag: hashtag_flag
+ * @hashtag_flag: if_flag
+ * @zero_flag: if_flag
+ * @minus_flag: if_flag
+ *
+ * @width: field
+ * @precision: field precision
+ *
+ * @h_modifier: h_modifier
+ * @1_modifier: on 1_modifier
+ *
+*/
+
+typedef struct parameters
+{
+	unsigned int unsign	: 1;
+
+	unsigned int plus_flag	: 1;
+	unsigned int space_flag	: 1;
+
+} specifier_t;
+
+int _puts(char *str);
+int _putchar(int c);
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_s(va_list ap, params_t *params);
+int _printf(va_list ap, params_t *params);
+
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, params_t *params, va_list ap);
+
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
+int print_from_to(char *start, char *stop,char *exc);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+void init_params(params_t *params, va_list ap);
+char *get_precision(char *p, params_t *params, va_list ap);
+int _printf(const char *format, ...);
+
+
+#endif
